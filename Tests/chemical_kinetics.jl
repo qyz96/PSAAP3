56dc968546e2de5491e04949aebfde1d095af028
qyz96 <qyz96@github.com>
2020-10-10 01:59:22 -0700

    add documentations for variables

--- Tests/chemical_kinetics.jl
+++ Tests/chemical_kinetics.jl
@@ -1,41 +1,39 @@
 using ADCME
-
-m = 1
-c_v = 1
+m = 1 # Total mass
 ### Constants: Chemical Kinetics Level
-V = 1
-N = 2
-M = 4
-ν1 = zeros(N,M)
-ν2 = zeros(N,M)
-ν = ν2 - ν1
-ΔS = ones(M)
-ΔH = ones(M)
-pa = 1
+V = 1  # Volume
+N = 2  # Number of Species
+M = 4  # Number of Reactions
+W = ones(N) # Molar weight
+ν1 = zeros(N,M) # Forward molar stoichiometric coefficients
+ν2 = zeros(N,M) # Backward model stoichiometric coefficients
+ν = ν2 - ν1  
+ΔS = ones(M)  # Enthalpy changes for reaction j
+ΔH = ones(M)  # Entahlpy for reaction j
+pa = 1 # 1 bar
+R = 8.31446261815324 # Gas constant
 ### Constants: Combustion Chamber Level
-min_dot = 1
-mout_dot = 1
-Yin = ones(N)
-Yout = ones(N)
-Qdot = 1
-hin = 1
-u = ones(N)
+min_dot = 1 # Rate at which mass enters the chamber
+mout_dot = 1 # Rate at which mass leaves the chamber
+Yin = ones(N) # Mass fraction of species entering the chamber
+Yout = ones(N) # Mass fraction of species leaving the chamber
+Qdot = 1 # Heating source
+hin = 1 # Enthalpy of input
 ### Unknowns
-T = 1
-W = ones(N)
-Y = ones(N)
-###
-ρ = m / V
-R = 1
-X = ρ * Y ./ W
-p = sum(X) * T
-Af = ones(M)
-β = ones(M)
-E = ones(M)
-Q = ones(M)
-Kf = ones(M)
-Kr = ones(M)
-
+T = 1 # Temperature
+Y = ones(N) # Mass fractions
+### Quantities that depend on the Unknowns
+u = 2.5 * R ./ W .* T   # Internal energy for species
+c_v = sum(2.5 * R .* Y ./ W) # Mass heat capacities
+ρ = m / V # density
+X = ρ * Y ./ W # Concentration
+p = sum(X) * T # pressure
+Af = ones(M) # preexponential constant Afj
+β = ones(M) # Temperature exponent
+E = ones(M) # Activation energy for the reactions
+Q = ones(M) # Individual progress rates
+Kf = ones(M) # Forward reaction coefficients
+Kr = ones(M) # Reverse reaction coefficients
 ##### Computing ω_dot 
 Kf = Af .* (T .^ β) .* exp.(-E ./ (R * T))
 Kr = Kf ./ (((pa/(R * T)) .^ sum(ν, dims=1)' .* exp.(ΔS ./ R - ΔH ./ (R * T))))
